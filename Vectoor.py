d6e4d68ade35f811243414b3eaa4b0002fd91b7f
Eduardobricenio <59741036+Eduardobricenio@users.noreply.github.com>
2020-07-23 00:00:48 -0500

    Update Vectoor.py

--- Vectoor.py
+++ Vectoor.py
@@ -1,1 +1,24 @@
-Jj
+
+In [1]: import numpy as np
+In [2]: a = np.array([[1, 2], [3, 4]])
+In [3]: a + a
+Out[3]:
+array([[2, 4],
+       [6, 8]])
+In [4]: m = np.matrix([[1, 2], [3, 4]])
+In [5]: m + m
+Out[5]:
+matrix([[2, 4],
+        [6, 8]])
+In [6]: a + m # Podemos sumar arrays y matrices
+Out[6]:
+matrix([[2, 4],
+        [6, 8]])
+In [7]: 2 * a # Producto por un escalar
+Out[7]:
+array([[2, 4],
+       [6, 8]])
+In [8]: -m
+Out[8]:
+matrix([[-1, -2],
+        [-3, -4]])
